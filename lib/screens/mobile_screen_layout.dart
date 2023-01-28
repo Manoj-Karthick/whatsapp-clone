ef54c0e7831d307c75ee318d600f00370264bca9
Manoj <manojkarthick1706@gmail.com>
2023-01-28 23:48:36 +0530

    Mobile Appbar

--- lib/screens/mobile_screen_layout.dart
+++ lib/screens/mobile_screen_layout.dart
@@ -1,12 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_ui/colors.dart';
 
 class MobileScreenLayout extends StatelessWidget {
   const MobileScreenLayout({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('In mobile screen')),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: appBarColor,
+          elevation: 0,
+          title: const Text(
+            'WhatsApp',
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.grey,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.search, color: Colors.grey),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.more_vert, color: Colors.grey),
+            ),
+          ],
+          bottom: const TabBar(
+            indicatorColor: tabColor,
+            indicatorWeight: 3,
+            labelColor: tabColor,
+            unselectedLabelColor: Colors.grey,
+            labelStyle: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.bold,
+            ),
+            tabs: [
+              Tab(
+                text: 'Chats',
+              ),
+              Tab(
+                text: 'Status',
+              ),
+              Tab(
+                text: 'Calls',
+              ),
+            ],
+          ),
+        ),
+        body: const Center(
+          child: Text('Mobile'),
+        ),
+      ),
     );
   }
 }
